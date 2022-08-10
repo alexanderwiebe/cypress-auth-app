3e3200aee08b3236e9711f0600210bcab997489c
Alexander Wiebe <alexanderjwiebe@gmail.com>
2022-08-10 10:09:49 -0600

    cy origin example of multiple domains

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,13 @@
 describe("My First Test", () => {
   it("Visits the initial project page", () => {
-    cy.visit("/");
-    cy.get("#sign-in").click();
+    cy.visit("http://localhost:2337/");
+    cy.origin("http://localhost:1337", () => {
+      cy.visit("/?url=http://localhost:2337");
+      cy.get('[formControlName="username"]').type("asdf");
+      cy.get('[formControlName="password"]').type("password");
+      cy.get("button").click();
+    });
+    cy.url().should("contain", "token");
+    cy.get("#success").should("exist");
   });
 });
